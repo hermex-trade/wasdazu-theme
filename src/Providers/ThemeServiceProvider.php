b1ddf308477b45802b60f90866e50ca624a4013c
Nicolas Almy <nicolas.almy@nicomania.ch>
2020-04-22 16:53:20 +0200

    refactoring

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -3,8 +3,6 @@
 namespace WasdazuTheme\Providers;
  
 use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\Events\Dispatcher;
-use Plenty\Plugin\Templates\Twig;
  
 class ThemeServiceProvider extends ServiceProvider
 {
