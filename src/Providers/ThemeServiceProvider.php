8febb0e01f78ea14168af73f36edec0ceaa6815d
Nicolas Almy <nicolas.almy@nicomania.ch>
2020-04-22 14:33:07 +0200

    added missing use directives

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -3,6 +3,8 @@
 namespace WasdazuTheme\Providers;
  
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\Templates\Twig;
  
 class ThemeServiceProvider extends ServiceProvider
 {
