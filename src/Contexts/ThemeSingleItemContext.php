2bb893d69520b7863c40199c060381ce9bc98014
Nicolas Almy <nicolas.almy@nicomania.ch>
2020-06-09 10:32:15 +0200

    added templates for similar and accesory item lists and a context provider

---
+++ src/Contexts/ThemeSingleItemContext.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace WasdazuTheme\Contexts;
+
+use IO\Helper\ContextInterface;
+use Ceres\Contexts\SingleItemContext;
+
+/**
+*  Bis Ceres 4 
+*/
+use IO\Services\ItemSearch\Services\ItemSearchService;    
+use IO\Services\ItemSearch\SearchPresets\CrossSellingItems;
+
+
+class ThemeNameSingleItemContext extends SingleItemContext implements ContextInterface
+{
+    public $accessory;
+    public $similar;
+
+    public function init($params)
+    {
+        parent::init($params);
+
+        $options = array(
+            "itemId" => $this->item['documents'][0]['data']['item']['id'],
+            "relation" => "Accessory"      // Nutze die Liste Zubehoer
+        );
+        $searchfactory = CrossSellingItems::getSearchFactory( $options );
+        $searchfactory->setPage(1, 4); // Begrenze auf 4 Artikel
+        $result = pluginApp(ItemSearchService::class)->getResult($searchfactory);
+        $this->accessory = $result['documents'];
+
+        $options = array(
+            "itemId" => $this->item['documents'][0]['data']['item']['id'],
+            "relation" => "Similar"      // Nutze die Liste Zubehoer
+        );
+        $searchfactory = CrossSellingItems::getSearchFactory( $options );
+        $searchfactory->setPage(1, 4); // Begrenze auf 4 Artikel
+        $result = pluginApp(ItemSearchService::class)->getResult($searchfactory);
+        $this->similar = $result['documents'];
+    }
+}
+
+
+?>
