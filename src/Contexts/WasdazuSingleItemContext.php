c8ccae0b7dbd647b3dc48ab46608ba8af80ba833
Nicolas Almy <nicolas.almy@nicomania.ch>
2020-06-09 15:03:35 +0200

    changed event listener to basket

--- src/Contexts/WasdazuSingleItemContext.php
+++ src/Contexts/WasdazuSingleItemContext.php
@@ -8,7 +8,7 @@ use Ceres\Contexts\SingleItemContext;
 use IO\Services\ItemSearch\Services\ItemSearchService;    
 use IO\Services\ItemSearch\SearchPresets\CrossSellingItems;
 
-class WasdazuSingleItemContext extends SingleItemContext implements ContextInterface
+class WasdazuSingleItemContext extends GlobalContext implements ContextInterface
 {
     public $accessory;
     public $similar;
